0032399621c891f7650289468bf6c82e7f2e8caa
melontini <104443436+melontini@users.noreply.github.com>
2023-10-24 16:08:08 +0700

    Unlock Knot.

--- src/main/java/me/melontini/forgerunner/loader/EarlyBootstrap.java
+++ src/main/java/me/melontini/forgerunner/loader/EarlyBootstrap.java
@@ -1,5 +1,6 @@
 package me.melontini.forgerunner.loader;
 
+import lombok.extern.slf4j.Slf4j;
 import me.melontini.forgerunner.loader.adapt.ModAdapter;
 import me.melontini.forgerunner.util.JarPath;
 import net.fabricmc.loader.api.LanguageAdapter;
@@ -7,14 +8,26 @@ import net.fabricmc.loader.api.LanguageAdapterException;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.impl.FabricLoaderImpl;
 import net.fabricmc.loader.impl.launch.FabricLauncherBase;
+import net.fabricmc.loader.impl.launch.knot.Knot;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
+@Slf4j
 public class EarlyBootstrap implements LanguageAdapter {
 
     static {
         //Dirty hack to not crash devenv with all the dependencies we use. (not sorry)
         FabricLoaderImpl.INSTANCE.getGameProvider().unlockClassPath(FabricLauncherBase.getLauncher());
+        try {
+            if (FabricLauncherBase.getLauncher() instanceof Knot knot) {
+                Field unlocked = Knot.class.getDeclaredField("unlocked");
+                unlocked.setAccessible(true);
+                unlocked.setBoolean(knot, true);
+            }
+        } catch (Throwable t) {
+            log.error("FAILED TO UNLOCK KNOT!!!", t);
+        }
 
         List<JarPath> forgeMods = ModLocator.start();
         ModAdapter.start(forgeMods);
