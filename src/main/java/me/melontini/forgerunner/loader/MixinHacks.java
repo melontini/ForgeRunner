3310ab2bc139a6b33dfe5224a7925edf5025b9a2
melontini <104443436+melontini@users.noreply.github.com>
2023-10-27 14:45:56 +0700

    Map srg names in the hacky bytecode provider

--- src/main/java/me/melontini/forgerunner/loader/MixinHacks.java
+++ src/main/java/me/melontini/forgerunner/loader/MixinHacks.java
@@ -2,6 +2,7 @@ package me.melontini.forgerunner.loader;
 
 import lombok.extern.slf4j.Slf4j;
 import me.melontini.forgerunner.loader.adapt.ModAdapter;
+import me.melontini.forgerunner.loader.remapping.SrgRemapper;
 import me.melontini.forgerunner.mod.Environment;
 import me.melontini.forgerunner.mod.ModClass;
 import me.melontini.forgerunner.util.Exceptions;
@@ -41,12 +42,12 @@ public class MixinHacks {
         IClassBytecodeProvider newProvider = new IClassBytecodeProvider() {
             final MappingResolver resolver = FabricLoader.getInstance().getMappingResolver();
             @Override public ClassNode getClassNode(String name) throws ClassNotFoundException, IOException {
-                if (name.startsWith("net/minecraft")) name = resolver.mapClassName("intermediary", name.replace("/", ".")).replace(".", "/");
+                if (name.startsWith("net/minecraft")) name = resolver.mapClassName("intermediary", SrgRemapper.mapClassName(name).replace("/", ".")).replace(".", "/");
                 try {return current.getClassNode(name);} catch (Throwable t) {return getNode(name);}
             }
 
             @Override public ClassNode getClassNode(String name, boolean runTransformers) throws ClassNotFoundException, IOException {
-                if (name.startsWith("net/minecraft")) name = resolver.mapClassName("intermediary", name.replace("/", ".")).replace(".", "/");
+                if (name.startsWith("net/minecraft")) name = resolver.mapClassName("intermediary", SrgRemapper.mapClassName(name).replace("/", ".")).replace(".", "/");
                 try {return current.getClassNode(name, runTransformers);} catch (Throwable t) {return getNode(name);}
             }
 
